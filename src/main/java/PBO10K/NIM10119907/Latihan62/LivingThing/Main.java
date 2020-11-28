7869b54886c62e81de0bd43233beb7358eaf316c
Aris Prabowo <aris@hijrstudio.com>
2020-11-28 19:02:15 +0700

    Initial Commit

---
+++ src/main/java/PBO10K/NIM10119907/Latihan62/LivingThing/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan62.LivingThing;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk menampilkan operasi living thing
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        var human = new Human();
+        human.setNama("Rizki Adam Kurniawan");
+        human.walk(human.getNama());
+        human.breath(human.getNama());
+        human.eat(human.getNama());
+    }
+    
+}
